49d95fd215db6ac074255e3111be1629e40dece1
Vishnu M K <mkv1722@gmail.com>
2022-03-29 12:35:14 +0530

    scripts ci/cd

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -7,5 +7,30 @@ $(document).ready(function() {
 
     $('#menu-icon').on('click', (e) => {
         $('.nav-right').toggleClass('nav-active')
-    }) 
+    });
+    
+    let ansField = $('#ans-field'),
+    imgField = $('#file-field'),
+    orText = $('#or-text');
+
+    ansField.on('keyup', function(e) {
+        if($(e.target).val() === '') {
+            imgField.removeClass('hidden');
+            orText.removeClass('hidden');
+            return;
+        }
+        imgField.addClass('hidden');
+        orText.addClass('hidden');
+    });
+
+    $('input[name=answerFile]').change(function(ev) {
+ 
+        if(imgField[0].files.length === 0) {
+            ansField.removeClass('hidden');
+            orText.removeClass('hidden');
+            return;
+        }
+        ansField.addClass('hidden');
+        orText.addClass('hidden');
+    });
 })
